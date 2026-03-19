d27939d153949c2e2675e89fc2258bb8a88d63ee
joostwmd <88150245+joostwmd@users.noreply.github.com>
2026-03-19 17:16:55 +0100

    setup: original source and thesis dir

--- llmorpheus/test/input/testProject/sorters/src/ISorter.ts
+++
@@ -1,6 +0,0 @@
-/**
- * An ISorter is an algorithm for sorting an array.
- */
-export default interface ISorter<E> {
-  sort(list: E[], compareFunction: (e1: E, e2: E) => number): void;
-}
--- llmorpheus/typings/espree.d.ts
+++
@@ -1,9 +0,0 @@
-// a minimal type definition file covering only what we need
-declare module "espree" {
-  export interface Options {
-    ecmaVersion?: number | "latest";
-    loc?: boolean;
-    comment?: boolean;
-  }
-  export function parse(code: string, options?: Options): any;
-}
